7f963d0bac889cf55e7494c1e7880dffd230b4ef
TheGrandUtochka <sublprod@gmail.com>
2023-08-21 20:48:00 +0500

    Исправлен ввод Task2, добавлена обработка исключения InputMismatchException

--- src/Task2/Task2.java
+++ src/Task2/Task2.java
@@ -1,21 +1,36 @@
 package Task2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.print("Введите значения перменных a и b: ");
+        int a = 0;
+        int b = 0;
+
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
-        System.out.println("До свапа: a = " + a + " b = " + b);
+        while (true) {
+            try {
+                System.out.print("Введите значения переменных a и b: ");
+                a = sc.nextInt();
+                b = sc.nextInt();
 
-        int[] swappedValues = valueSwap(a, b);
-        a = swappedValues[0];
-        b = swappedValues[1];
+                System.out.println("До свапа: a = " + a + " b = " + b);
 
-        System.out.println("После свапа: a = " + a + " b = " + b);
+                int[] swappedValues = valueSwap(a, b);
+                a = swappedValues[0];
+                b = swappedValues[1];
+
+                System.out.println("После свапа: a = " + a + " b = " + b);
+
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Ну вот ни стыда, ни совести! Нужно ввести целочисленные значения a и b! Например [-2, 5], [3, 2] и т.д..");
+                sc.nextLine();
+            }
+        }
+        sc.close();
     }
 
     static int[] valueSwap(int a, int b) {
